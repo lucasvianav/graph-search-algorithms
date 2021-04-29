1dd81f768b86b76fd501819d9c6a0e82e5d98079
Lucas Viana <oviana@outlook.com>
2021-04-29 10:20:02 -0300

    ex01 (erdös-renyi matrix) - conversion to class

--- ex01/main.py
+++ ex01/main.py
@@ -1,26 +1,42 @@
 from random import random
 from math import comb
 
-# Return a Erdös-Renyi graph's matrix (considering non-directional graphs)
-# N if the graph's number of vertices
-# p is the Erdös-Renyi parameter
-def genErdösRenyiMatrix(N: int, p: float):
-    M = [ [ 0 for _ in range(N) ] for _ in range(N) ]
-    for i in range(N):
-        for j in range(N):
-            M[i][j] = M[j][i] = ( int(random() > p) if i > j else M[i][j] )
+class Graph:
+    # Generates a Erdös-Renyi graph's matrix (considering non-directional graphs)
+    # N if the graph's number of vertices
+    # p is the Erdös-Renyi parameter
+    def __init__(self, N, p=None):
+        M = [ [ 0 for _ in range(N) ] for _ in range(N) ]
 
-    return M
+        if p != None:
+            for i in range(N):
+                for j in range(N):
+                    M[i][j] = M[j][i] = ( int(random() > p) if i > j else M[i][j] )
 
+        self.N = N
+        self.p = p
+        self.matrix = M
+        
+    def __str__(self) -> str:
+        string = ''
+
+        for i in range(self.N):
+            string += '[  '
+            for j in range(self.N): string += str(self.matrix[i][j]) + '  '
+            string += ']\n'
+            
+        return string
+        
+    def getMatrix(self): return self.matrix
+    
+    def existsEdge(self):
+        for row in self.matrix:
+            if any(x > 0 for x in row): return True
+
+    # def getVertexDegree(self, vertexIndex):
+        
 
 # Receives user input and generates the matrix
-noVertices = int(input('Insert number of vertices (N): '))
-parameter = float(input('Insert the Erdös-Renyi parameter (p): '))
-matrix = genErdösRenyiMatrix(noVertices, parameter)
-
-# Prints the matrix
-for i in range(noVertices):
-    print('[', end='  ')
-    for j in range(noVertices):
-        print(matrix[i][j], end='  ')
-    print(']')
+# noVertices = int(input('Insert number of vertices (N): '))
+# parameter = float(input('Insert the Erdös-Renyi parameter (p): '))
+# matrix = genErdösRenyiMatrix(noVertices, parameter)
