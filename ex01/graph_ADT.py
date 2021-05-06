e3229695270efacd5c4f6405bcd4b82b97b89bf2
Lucas Viana <oviana@outlook.com>
2021-05-06 19:39:16 -0300

    Graph ADT added docstrings and function annotations

--- ex01/graph_ADT.py
+++ ex01/graph_ADT.py
@@ -3,10 +3,24 @@ from random import random
 from functools import reduce
 
 class Graph:
-    # N if the graph's number of vertices
-    # p is the Erdös-Renyi parameter
-    # M is a graph's matrix (will be used if no p is given)
+    """ This is a class for graphs manipulation, primarily working with it's adjacency matrix.  """
+
     def __init__(self, N: int, p=None, M=None):
+        """
+        The constructor for the Graph class.
+
+        If p is passed, an Erdös-Renyi matrix is used. Else, M will be used.
+
+        Parameters:
+            N (int): The graph's number of vertices.
+            p (float, default None): The Erdös-Renyi parameter.
+            M ([ [ int ] ], default None): The graph's adjacency matrix.
+
+        Return value:
+            Graph, if the passed parameters are valid.
+            None, if not.
+        """
+
         M = [ [ 0 for _ in range(N) ] for _ in range(N) ]
 
         # if p is passed, generates an Erdös-Renyi adjacency matrix (considering non-directional graphs)
@@ -24,6 +38,13 @@ class Graph:
         self.matrix = M
 
     def __str__(self) -> str:
+        """
+        The function to convert a graph into a string of it's adjacency matrix.
+
+        Return value:
+            str: represents the graph's adjacency matrix.
+        """
+
         string = ''
 
         for i in range(self.N):
@@ -33,10 +54,20 @@ class Graph:
 
         return string
 
-    def getMatrix(self): return self.matrix
+    def getMatrix(self): 
+        """ The getter for the graph's adjacency matrix.  """
+        return self.matrix
 
-    # checks if the graph has at least one edge
-    def hasEdge(self):
+    def hasEdge(self) -> bool:
+        """
+        The function for checking if the graph has at least one edge.
+
+        Return value: 
+            bool, if vertexPosition is valid (between 1 and self.N).
+            None, if not.
+        """
+
+
         # If an edge is found in any row
         # returns True
         for row in self.matrix:
@@ -45,27 +76,69 @@ class Graph:
         # If no edge is found, returns False
         return False
 
-    # returns the vertex's degree
-    def getVertexDegree(self, vertexPosition: int):
+    def getVertexDegree(self, vertexPosition: int) -> int:
+        """
+        The function for checking a vertex's degree.
+
+        Parameters:
+            vertexPosition (int): The queried vertex's position (index + 1).
+
+        Return value: 
+            int, if vertexPosition is valid (between 1 and self.N).
+            None, if not.
+
+        """
+
         # Sums all edges from the vertex's row
         return reduce(lambda acc, cur: acc+cur, self.matrix[vertexPosition-1], 0) if 1 <= vertexPosition <= self.N else None
     
-    # returns the vertex's list of adjacent vertices
     def getAdjacentVertices(self, vertexPosition: int):
+        """
+        The function for getting a vertex's list of adjacent vertices.
+
+        Parameters:
+            vertexPosition (int): The queried vertex's position (index + 1).
+
+        Return value: 
+            [ int ], if vertexPosition is valid (between 1 and self.N).
+                List of adjacent vertices' positions (index + 1).
+            None, if not.
+        """
+
         return list(filter(
             # filter list for indexes
             lambda e: e != None, 
             
-            # generates list of indexes (for the adjacent vertices) and None for the non-adjacent vertices
+            # generates list of indexes+1 (for the adjacent vertices) and None for the non-adjacent vertices
             [ vertex+1 if isAdjacent else None for vertex, isAdjacent in enumerate(self.matrix[vertexPosition-1]) ]
         )) if 1 <= vertexPosition <= self.N else None
 
-    # returns the vertex's degree and list of adjacency
     def getVertexInfo(self, vertexPosition: int):
+        """
+        The function for getting both a vertex's degree and it's list of adjacent vertices.
+
+        Parameters:
+            vertexPosition (int): The queried vertex's position (index + 1).
+
+        Return value:
+            ( int, [ int ] ), if vertexPosition is valid (between 1 and self.N).
+                Represents the vertex degree and it's list of adjacent vertices' positions (index + 1), respectively.
+            None, if not.
+        """
+
         return self.getVertexDegree(self, vertexPosition), self.getAdjacentVertices(vertexPosition) if 1 <= vertexPosition <= self.N else None
 
-    # checks if the vertex in position i is adjacent to the vertex in position j
-    # (checks if there is at least an edge between the two vertices)
-    # returns None if either i or j is invalid
-    def areAdjacents(self, i: int, j: int):
+    def areAdjacent(self, i: int, j: int) -> bool:
+        """
+        The function for checking if two vertices are adjacent (e.g.: if there is at least an edge between them).
+
+        Parameters:
+            i (int): The first queried vertex's position (index + 1).
+            j (int): The second queried vertex's position (index + 1).
+
+        Return value: 
+            bool, if i and j are valid (between 1 and self.N).
+            None, if not.
+        """
+
         return bool(self.matrix[i-1][j-1] or self.matrix[j-1][i-1]) if 1 <= i <= self.N and 1 <= j <= self.N else None
