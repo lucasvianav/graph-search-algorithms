9b6743291995b9dac6bf032fdb01a1fcc98b6e8e
Lucas Viana <oviana@outlook.com>
2021-06-14 21:14:14 -0300

    knn graph constructor

--- graph.py
+++ graph.py
@@ -1,59 +1,70 @@
-from functools import reduce
-from os import path
+from math import sqrt
 from queue import Queue
+from random import random
+
 
 class Graph:
-    """ This is a class for graphs manipulation, primarily working with it's adjacency list.  """
+    """
+    This is a class for testing search algorithms for graphs.
 
-    def __init__(self, filePath: str) -> list:
+    Class constructor - generates a random KNN Graph.
+
+    Parameters:
+            nVertices (int): number of random vertices to be generated.
+            nEdges (int): number of edges to be generated from each vertex.
+    """
+
+    def __init__(self, nVertices: int, nEdges: int):
         """
-        The constructor for the Graph class.
+        Class constructor - generates a random KNN Graph.
 
         Parameters:
-            filePath (str): The pajek file's path
-
-        Return value:
-            Graph, if the passed parameters are valid.
-            None, if not.
+            nVertices (int): number of random vertices to be generated.
+            nEdges (int): number of edges to be generated from each vertex.
         """
 
-        # reads the pajek file
-        if path.exists(filePath):
-            with open(filePath, 'r') as f: lines = f.read().splitlines()
+        vertices = [ ( nVertices*random(), nVertices*random() ) for _ in range(nVertices) ]
+        edges = [ [ 0 for _ in range(nVertices) ] for _ in range(nVertices) ]
+        adjacencies = []
 
-        else: return None
-        
-        # number of vertices
-        N = int(lines[0].replace('*Vertices ', ''))
-    
-        # deletes the files' header
-        lines.pop(0)
-        lines.pop(0)
-        
-        # this function will be used to reduce the "lines" list
-        def parseData(acc, cur):
-            currentVertex, adjacentVertex = cur.split(' ')
+        def findClosestVertices(current: tuple) -> list:
+            """
+            Calculates the vertex that's closest from the current one.
 
-            # parses int from string
-            currentVertex = int(currentVertex)
-            adjacentVertex = int(adjacentVertex)
-            
-            # if the current vertex is already listed on the adjacency list,
-            # add the current adjacent vertex to it
-            if len(acc) and acc[-1]['vertex'] == currentVertex: acc[-1]['adjacencies'].append(adjacentVertex)
+            Parameters:
+                current (int): the current vertex' index in the "vertices" list.
 
-            # if it's not, add it to the adjacency list with only the current adjacent vertex
-            else: acc.append({ 'vertex': currentVertex, 'adjacencies': [ adjacentVertex ] })
-            
-            return acc
-        
-        # the adjacency list will be formated as a list of objects in which each object 
-        # has a "vertex" attribute indicating it's position as well a "adjacencies" list 
-        # indicating to what other vertex that one is adjacent
-        # [ { vertex: int, adjacencies: [ int ] } ]
+            Return value:
+                int: the closes vertex' index in the "vertices list".
+            """
 
-        self.N = N
-        self.adjacencies = reduce(parseData, lines, [])
+            # list of distances between all vertices and the current one
+            # (does not include the distance with itself)
+            distancies = [
+                ( i, sqrt((vertex[0] - current[0])**2 + (vertex[1] - current[1])**2) )
+                for i, vertex in enumerate(vertices) if vertex != current
+            ]
+
+            # sorts by distance
+            return sorted(distancies, key=lambda vertex: vertex[1])[0:nEdges]
+
+        for current_index, current_vertex in enumerate(vertices):
+            # gets "nEdges" closest vertices
+            closest = findClosestVertices(current_vertex)
+
+            # for each of the closest vertices, create an edge between them
+            for vertex_index, _ in closest:
+                edges[current_index][vertex_index] = edges[vertex_index][current_index] = 1
+
+        for current_vertex, row in enumerate(edges): adjacencies.append({
+            "vertex": current_vertex,
+            "adjacencies": [ vertex for vertex, hasEdge in enumerate(row) if hasEdge ]
+        })
+
+        self.nVertices = nVertices
+        self.vertices = vertices
+        # self.edges = edges
+        self.adjacencies = adjacencies
 
     def __str__(self) -> str:
         """
@@ -70,74 +81,66 @@ class Graph:
 
         return string
 
-    def getAdjacencyList(self) -> list: 
-        """ The getter for the graph's adjacency list.  """
-        return self.adjacencies
-
-    def getAdjacentVertices(self, vertexPosition: int):
+    def getAdjacentVertices(self, index: int):
         """
-        The function for getting a vertex's adjacency list.
+        The function for getting a vertex's adjacent vertices.
 
         Parameters:
-            vertexPosition (int): The queried vertex's position (index + 1).
+            index (int): The queried vertex's index.
 
-        Return value: 
-            [ int ], if vertexPosition is valid (between 1 and self.N).
-                List of adjacent vertices' positions (index + 1).
-            None, if not.
+        Return value:
+            list<int>: list of adjacent vertices' index.
         """
-        if not (1 <= vertexPosition <= self.N): return None
 
-        vertexObject = list(filter(lambda v: v['vertex'] == vertexPosition, self.adjacencies))
-        adjacencies = vertexObject[0]['adjacencies'] if len(vertexObject) else []
+        vertexFiltered = [ v for v in self.adjacencies if v["vertex"] == index ]
 
-        return adjacencies
+        return vertexFiltered[0]['adjacencies'] if len(vertexFiltered) else []
 
-    def breadthFirstSearch(self, vertexPosition: int) -> tuple:
+    def breadthFirstSearch(self, index: int) -> tuple:
         """
-        The function performs a Breadth First Search, returning the list of distances between the vertex at "vertexPosition" and every other vertex the graph has as well of the list of all visited vertices (in the order they were visited).
+        The function performs a Breadth First Search, returning the list of distances between the vertex at "index" and every other vertex the graph has as well of the list of all visited vertices (in the order they were visited).
 
         Parameters:
-            vertexPosition (int): The initial vertex's position (index + 1).
+            index (int): The initial vertex's position (index + 1).
 
-        Return value: 
-            tuple, ([int], [int]): (visitedVertices, distances).
+        Return value:
+            tuple<[int],[int]>: (visitedVertices, distances).
 
              * visitedVertexes, [ int ]: list containing all of the visited vertices' positions in the order they were visited.
-             * distances, [ int ]: list in which the indexes are the vertices' positions - 1 and the values are their distances to the vertex at "vertexPosition".
-             
-            If vertexPosition is invalid, None will be returned.
+             * distances, [ int ]: list in which the indexes are the vertices' positions - 1 and the values are their distances to the vertex at "index".
 
+            If index is invalid, None will be returned.
         """
-        if not (1 <= vertexPosition <= self.N): return None
+
+        if not (1 <= index <= self.nVertices): return None
 
         # list of all vertices not yet passed (analyzed)
-        white = list(range(1, self.N+1))
+        white = list(range(1, self.nVertices+1))
 
         # adjacent vertex to the ones already passed
         # they're queued to be analyzed
         grey = Queue()
-        
+
         # list of all vertices already passed (analyzed) in order
         black = []
-        
+
         # starts the distances list as -1 for all vertices
         # (-1 in this case means infinity)
         distances = [ -1 for _ in white ]
 
-        grey.put(vertexPosition)
+        grey.put(index)
         distances[vertexPosition-1] = 0
-        
+
         # while the queue is not empty
         while grey.qsize():
             black.append(grey.get())
-            
+
             # gets the vertex currently being analyzed vertex's adjacency list
             adjacencies = self.getAdjacentVertices(black[-1])
-           
+
             # loops through the currently being analyzed vertex's adjacency list
             for vertex in adjacencies:
-                
+
                 # looks through any adjacent vertices that were not analyzed yet
                 if vertex in white:
                     # removes them from the white list
@@ -145,7 +148,7 @@ class Graph:
 
                     # adds them to the grey list
                     grey.put(vertex)
-                    
+
                     # marks their distances as an increment of the currently being analyzed vertex's
                     distances[vertex-1] = distances[black[-1]-1] + 1
 
