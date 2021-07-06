4e10a8a5a58494398ccb7b06de771bf09be88222
Lucas Viana <oviana@outlook.com>
2021-07-06 20:32:20 -0300

    makes main calculate time and distance average

--- main.py
+++ main.py
@@ -1,72 +1,196 @@
 from random import randint
+from statistics import mean
 from time import time
 
 from graph import Graph
 
-# no_vertices = [ 500 ]
 no_vertices = [ 500, 5000, 10000 ]
 no_edges    = [ 3,   5,    7     ]
 
-filename = './output/' + str(time()).replace('.','')
+OUTPUT_DIR = './output/'
+FILENAME = OUTPUT_DIR + str(time()).replace('.','')
 
-with open(filename + '.out', 'w') as f:
-    for n in no_vertices:
-        root   = randint(0, n-1)
-        target = randint(0, n-1)
+data = {
+    'breadth': {
+        500: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
 
-        for k in no_edges:
-            graph = Graph(n, k)
+        5000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        10000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        }
+    },
+
+    'depth': {
+        500: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        5000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        10000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        }
+    },
+
+    'best': {
+        500: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        5000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        10000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        }
+    },
+
+    'a': {
+        500: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        5000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        10000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        }
+    },
+
+    'a_star': {
+        500: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        5000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        },
+
+        10000: {
+            3: { 'times': [], 'distances': [] },
+            5: { 'times': [], 'distances': [] },
+            7: { 'times': [], 'distances': [] }
+        }
+    }
+}
+
+NO_SAMPLES = 10
+
+def avg(algorithm: str, n: int, k: int, list_type: str):
+    return mean(data[algorithm][n][k][list_type])
+
+for n in no_vertices:
+    for k in no_edges:
+        graph = Graph(n, k)
+
+        for _ in range(NO_SAMPLES):
+            root   = randint(0, n-1)
+            target = randint(0, n-1)
 
             while target == root: target = randint(0, n-1)
 
+
+            # BREADTH FIRST SEARCH
             start_time   = time()
             breadth      = graph.breadthFirstSearch(root, target)
             breadth_time = time() - start_time
 
+            data['breadth'][n][k]['distances'].append(graph.travelledDistance(breadth))
+            data['breadth'][n][k]['times'].append(breadth_time)
+
+
+            # DEPTH FIRST SEARCH
             start_time   = time()
             depth        = graph.depthFirstSearch(root, target)
             depth_time   = time() - start_time
 
+            data['depth'][n][k]['distances'].append(graph.travelledDistance(depth))
+            data['depth'][n][k]['times'].append(depth_time)
+
+
+            # BEST FIRST SEARCH
             start_time   = time()
             best         = graph.bestFirstSearch(root, target)
             best_time    = time() - start_time
 
+            data['best'][n][k]['distances'].append(graph.travelledDistance(best))
+            data['best'][n][k]['times'].append(best_time)
+
+
+            # A ALGORITHM
             start_time   = time()
             a            = graph.aSearch(root, target)
             a_time       = time() - start_time
 
+            data['a'][n][k]['distances'].append(graph.travelledDistance(a))
+            data['a'][n][k]['times'].append(a_time)
+
+
+            # A* ALGORITHM
             start_time   = time()
             a_star       = graph.aStarSearch(root, target)
             a_star_time  = time() - start_time
 
-            f.write(f'\n\n\n\n###  n = {n}  ###  k = {k}  ### root = {root}  ###  target = {target}  ###\n')
-            f.write(f'Breadth First Search: {breadth_time}s\n')
-            f.write(str(breadth) + '\n\n')
-            f.write(f'Depth First Search: {depth_time}s\n')
-            f.write(str(depth) + '\n\n')
-            f.write(f'Best First Search: {best_time}s\n')
-            f.write(str(best) + '\n\n')
-            f.write(f'A Search: {a_time}s\n')
-            f.write(str(a) + '\n\n')
-            f.write(f'A Star Search: {a_star_time}s\n')
-            f.write(str(a_star) + '\n\n')
+            data['a_star'][n][k]['distances'].append(graph.travelledDistance(a_star))
+            data['a_star'][n][k]['times'].append(a_star_time)
 
+        print(f'###  n = {n}  ###  k = {k}  ###', end='\n\n')
 
-            print(f'\n\n\n\n###  n = {n}  ###  k = {k}  ### root = {root}  ###  target = {target}  ###\n')
-            print(f'Breadth First Search: {breadth_time}s')
-            print(breadth, end='\n\n')
-            print(f'Depth First Search: {depth_time}s')
-            print(depth, end='\n\n')
-            print(f'Best First Search: {best_time}s')
-            print(best, end='\n\n')
-            print(f'A Search: {a_time}s')
-            print(a, end='\n\n')
-            print(f'A Star Search: {a_star_time}s')
-            print(a_star, end='\n\n')
+        print(f'Breadth First Search:')
+        print(' * Time: %.2es', avg('breadth', n, k, 'times'))
+        print(' * Distance: %.2e', avg('breadth', n, k, 'distances'), end='\n\n')
 
-            graph.plot(f'{filename}-breadth-{n}-{k}' , breadth)
-            graph.plot(f'{filename}-depth-{n}-{k}'   , depth)
-            graph.plot(f'{filename}-best-{n}-{k}'    , best)
-            graph.plot(f'{filename}-a-{n}-{k}'       , a)
-            graph.plot(f'{filename}-a_star-{n}-{k}'  , a_star)
+        print(f'Depth First Search:')
+        print(' * Time: %.2es', avg('depth', n, k, 'times'))
+        print(' * Distance: %.2e', avg('depth', n, k, 'distances'), end='\n\n')
+
+        print(f'Best First Search:')
+        print(' * Time: %.2es', avg('best', n, k, 'times'))
+        print(' * Distance: %.2e', avg('best', n, k, 'distances'), end='\n\n')
+
+        print(f'A Search:')
+        print(' * Time: %.2es', avg('a', n, k, 'times'))
+        print(' * Distance: %.2e', avg('a', n, k, 'distances'), end='\n\n')
+
+        print(f'A Star Search:')
+        print(' * Time: %.2es', avg('a_star', n, k, 'times'))
+        print(' * Distance: %.2e', avg('a_star', n, k, 'distances'), end='\n\n\n\n\n')
 
