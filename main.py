bab9204dc33fac9a904e471bc7d14b6c877e9ed9
Lucas Viana <oviana@outlook.com>
2021-07-06 20:42:30 -0300

    main - optimizes data storing

--- main.py
+++ main.py
@@ -11,111 +11,17 @@ OUTPUT_DIR = './output/'
 FILENAME = OUTPUT_DIR + str(time()).replace('.','')
 
 data = {
-    'breadth': {
-        500: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        5000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        10000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        }
-    },
-
-    'depth': {
-        500: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        5000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        10000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        }
-    },
-
-    'best': {
-        500: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        5000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        10000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        }
-    },
-
-    'a': {
-        500: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        5000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        10000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        }
-    },
-
-    'a_star': {
-        500: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        5000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        },
-
-        10000: {
-            3: { 'times': [], 'distances': [] },
-            5: { 'times': [], 'distances': [] },
-            7: { 'times': [], 'distances': [] }
-        }
-    }
+    'breadth': { 'times': [], 'distances': [] },
+    'depth': { 'times': [], 'distances': [] },
+    'best': { 'times': [], 'distances': [] },
+    'a': { 'times': [], 'distances': [] },
+    'a_star': { 'times': [], 'distances': [] },
 }
 
 NO_SAMPLES = 10
 
-def avg(algorithm: str, n: int, k: int, list_type: str):
-    return mean(data[algorithm][n][k][list_type])
+def avg(algorithm: str, list_type: str):
+    return mean(data[algorithm][list_type])
 
 for n in no_vertices:
     for k in no_edges:
@@ -133,8 +39,8 @@ for n in no_vertices:
             breadth      = graph.breadthFirstSearch(root, target)
             breadth_time = time() - start_time
 
-            data['breadth'][n][k]['distances'].append(graph.travelledDistance(breadth))
-            data['breadth'][n][k]['times'].append(breadth_time)
+            data['breadth']['distances'].append(graph.travelledDistance(breadth))
+            data['breadth']['times'].append(breadth_time)
 
 
             # DEPTH FIRST SEARCH
@@ -142,8 +48,8 @@ for n in no_vertices:
             depth        = graph.depthFirstSearch(root, target)
             depth_time   = time() - start_time
 
-            data['depth'][n][k]['distances'].append(graph.travelledDistance(depth))
-            data['depth'][n][k]['times'].append(depth_time)
+            data['depth']['distances'].append(graph.travelledDistance(depth))
+            data['depth']['times'].append(depth_time)
 
 
             # BEST FIRST SEARCH
@@ -151,8 +57,8 @@ for n in no_vertices:
             best         = graph.bestFirstSearch(root, target)
             best_time    = time() - start_time
 
-            data['best'][n][k]['distances'].append(graph.travelledDistance(best))
-            data['best'][n][k]['times'].append(best_time)
+            data['best']['distances'].append(graph.travelledDistance(best))
+            data['best']['times'].append(best_time)
 
 
             # A ALGORITHM
@@ -160,8 +66,8 @@ for n in no_vertices:
             a            = graph.aSearch(root, target)
             a_time       = time() - start_time
 
-            data['a'][n][k]['distances'].append(graph.travelledDistance(a))
-            data['a'][n][k]['times'].append(a_time)
+            data['a']['distances'].append(graph.travelledDistance(a))
+            data['a']['times'].append(a_time)
 
 
             # A* ALGORITHM
@@ -169,28 +75,28 @@ for n in no_vertices:
             a_star       = graph.aStarSearch(root, target)
             a_star_time  = time() - start_time
 
-            data['a_star'][n][k]['distances'].append(graph.travelledDistance(a_star))
-            data['a_star'][n][k]['times'].append(a_star_time)
+            data['a_star']['distances'].append(graph.travelledDistance(a_star))
+            data['a_star']['times'].append(a_star_time)
 
-        print(f'###  n = {n}  ###  k = {k}  ###', end='\n\n')
+        print(f"###  n = {n}  ###  k = {k}  ###', end='\n\n")
 
-        print(f'Breadth First Search:')
-        print(' * Time: %.2es', avg('breadth', n, k, 'times'))
-        print(' * Distance: %.2e', avg('breadth', n, k, 'distances'), end='\n\n')
+        print(f"Breadth First Search:")
+        print(f" * Time: {avg('breadth', 'times'):.2e}s")
+        print(f" * Distance: {avg('breadth', 'distances'):.2e}', end='\n\n")
 
-        print(f'Depth First Search:')
-        print(' * Time: %.2es', avg('depth', n, k, 'times'))
-        print(' * Distance: %.2e', avg('depth', n, k, 'distances'), end='\n\n')
+        print(f"Depth First Search:")
+        print(f" * Time: {avg('depth', 'times'):.2e}s")
+        print(f" * Distance: {avg('depth', 'distances'):.2e}', end='\n\n")
 
-        print(f'Best First Search:')
-        print(' * Time: %.2es', avg('best', n, k, 'times'))
-        print(' * Distance: %.2e', avg('best', n, k, 'distances'), end='\n\n')
+        print(f"Best First Search:")
+        print(f" * Time: {avg('best', 'times'):.2e}s")
+        print(f" * Distance: {avg('best', 'distances'):.2e}', end='\n\n")
 
-        print(f'A Search:')
-        print(' * Time: %.2es', avg('a', n, k, 'times'))
-        print(' * Distance: %.2e', avg('a', n, k, 'distances'), end='\n\n')
+        print(f"A Search:")
+        print(f" * Time: {avg('a', 'times'):.2e}s")
+        print(f" * Distance: {avg('a', 'distances'):.2e}', end='\n\n")
 
-        print(f'A Star Search:')
-        print(' * Time: %.2es', avg('a_star', n, k, 'times'))
-        print(' * Distance: %.2e', avg('a_star', n, k, 'distances'), end='\n\n\n\n\n')
+        print(f"A Star Search:")
+        print(f" * Time: {avg('a_star', 'times'):.2e}s")
+        print(f" * Distance: {avg('a_star', 'distances'):.2e}', end='\n\n\n\n\n")
 
